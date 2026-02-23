21f6b6c5464dab65261db1b450dad6a664583c9a
Rich Scott <richscott@sent.com>
2026-02-23 16:14:58 -0700

    Walk a directory hierarchy, process all .jar files
    Signed-off-by: Rich Scott <richscott@sent.com>

--- main.go
+++ main.go
@@ -3,20 +3,47 @@ package main
 import (
 	"archive/zip"
 	"fmt"
+	"io/fs"
 	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
 )
 
-func main() {
-	jarfile := "/Users/richscott/.m2/repository/org/apache/spark/spark-core_2.13/3.5.5/spark-core_2.13-3.5.5.jar"
+var classRe = regexp.MustCompile(`\.class$`)
 
-	// Open a zip archive for reading.
-	r, err := zip.OpenReader(jarfile)
-	if err != nil {
-		log.Fatal(err)
+var extractClasses = func(path string, d fs.DirEntry, err error) error {
+	if d.IsDir() || !strings.HasSuffix(path, ".jar") {
+		return nil
 	}
-	defer r.Close()
 
+	r, openErr := zip.OpenReader(path)
+	if openErr != nil {
+		log.Fatal(openErr)
+	}
+	defer func() {
+		closeErr := r.Close()
+		if closeErr != nil {
+			fmt.Fprintf(os.Stderr, "error closing jar file reader: %v\n", closeErr)
+		}
+	}()
+
+	fmt.Printf("%s\n", path)
 	for _, f := range r.File {
-		fmt.Printf("%s\n", f.Name)
+		if classRe.MatchString(f.Name) {
+			fmt.Printf("%s\n", f.Name)
+		}
+	}
+	fmt.Printf("\n")
+	return nil
+}
+
+func main() {
+	jarroot := "/Users/richscott/.m2/repository/org/apache/spark/spark-core_2.13/3.5.5"
+
+	err := filepath.WalkDir(jarroot, extractClasses)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error searching path: %v\n", err)
 	}
 }
